be9e51bd80c885132ad6f2fb7758b3bef93a6d92
Rob <robert.koskulics@me.com>
2017-12-21 09:56:52 -0500

    Variables all working for times before now

--- assets/train.js
+++ assets/train.js
@@ -16,13 +16,15 @@ $("#add-train-button").on("click", function(event) {
   // Gets the values the user inputs and saves them as variables
   var trainName = $("#train-name-input").val().trim();
   var trainDestination = $("#train-destination-input").val().trim();
-  var trainTime = moment($("#train-time-input").val().trim(), "HH:mm").format("X");
+  var trainTime = moment($("#train-time-input").val().trim(), "HH:mm").format("HH:mm");
+  var trainTimeUnix = moment($("#train-time-input").val().trim(), "HH:mm").format("X");
   var trainFrequency = $("#train-frequency-input").val().trim();
   // Creates a temporary object to send to firebase
   var newTrain = {
     train: trainName,
     destination: trainDestination,
     time: trainTime,
+    timeUnix: trainTimeUnix,
     frequency: trainFrequency
   };
   
@@ -43,20 +45,28 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey) {
   var trainName = childSnapshot.val().train;
   var trainDestination = childSnapshot.val().destination;
   var trainTime = childSnapshot.val().time;
+  var trainTimeUnix = childSnapshot.val().timeUnix;
   var trainFrequency = childSnapshot.val().frequency;
 
 // Need to calculate time to next train.  The equation is in the format:
 // ((now-trainTime) % frequency) would give us the remainder.  Frequency - remainder + Now gives us the next train time.
 // Frequency - Remainder gives us time till the next train
-var now = moment().format("X");
-var difference = now - trainTime;
+var nowUnixTime = moment().format("X");
+var nowMilitaryTime = moment().format("HH:mm");
+var difference = nowUnixTime - trainTimeUnix;
 var differenceInMinutes = difference / 60;
+differenceInMinutes = Math.floor(differenceInMinutes);
+var remainder = differenceInMinutes % trainFrequency;
+var minutesAway = trainFrequency - remainder;
+var nextTrainUnix = moment().add(minutesAway, "minutes");
+var nextTrain = moment(nextTrainUnix).format("HH:mm");
 
 
-console.log(differenceInMinutes);
+
+console.log(differenceInMinutes, remainder);
 
 
   // Puts the data into the HTML
   $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" +
-  trainFrequency + "</td><td>" + "nextTrain" + "</td><td>" + "minutesAway" + "</td></tr>");
+  trainFrequency + "</td><td>" + nextTrain + "</td><td>" + minutesAway + "</td></tr>");
 });
